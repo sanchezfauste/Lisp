e9080061df4e9af7a7503ea537c20fb1d63a074e
Marc Sánchez <sanchezfauste@gmail.com>
2016-02-01 22:22:20 +0100

    Implementada classe ConsCell

--- src/main/java/cat/udl/eps/butterp/data/ConsCell.java
+++ src/main/java/cat/udl/eps/butterp/data/ConsCell.java
@@ -1,6 +1,8 @@
 package cat.udl.eps.butterp.data;
 
 import cat.udl.eps.butterp.environment.Environment;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConsCell implements SExpression {
 
@@ -8,26 +10,55 @@ public class ConsCell implements SExpression {
     public final SExpression cdr; // Si el definiu privat caldrà un getter
 
     public ConsCell(SExpression car, SExpression cdr) {
-        throw new UnsupportedOperationException("not implemented yet");
+        this.car = car;
+        this.cdr = cdr;
     }
 
     @Override
     public SExpression eval(Environment env) {
-        throw new UnsupportedOperationException("not implemented yet");
+        SExpression result = car.eval(env);
+        if (result instanceof Function) {
+            List<SExpression> evargs = new ArrayList<SExpression>();
+            SExpression next = cdr;
+            while (!next.equals(Symbol.NIL)) {
+                ConsCell cell = (ConsCell) next;
+                evargs.add(cell.car.eval(env));
+                next = cell.cdr;
+            }
+            return ((Function) result).apply(ListOps.list(evargs), env);
+        }
+        if (result instanceof Special) {
+            return ((Special) result).applySpecial(cdr, env);
+        }
+        throw new EvaluationError("Cannot apply " + result + ".");
     }
 
     @Override
     public boolean equals(Object o) {
-        throw new UnsupportedOperationException("not implemented yet");
+        if (this == o) {
+            return true;
+        }
+        if (o instanceof ConsCell) {
+            return this.car.equals(((ConsCell) o).car)
+                    && this.cdr.equals(((ConsCell) o).cdr);
+        }
+        return false;
     }
 
     @Override
     public int hashCode() {
-        throw new UnsupportedOperationException("not implemented yet");
+        return 3 * car.hashCode() + 7 * cdr.hashCode();
     }
 
     @Override
     public String toString() {
-        throw new UnsupportedOperationException("not implemented yet");
+        String result = "(" + car;
+        SExpression next = cdr;
+        while (!next.equals(Symbol.NIL)) {
+            ConsCell cell = (ConsCell) next;
+            result += " " + cell.car;
+            next = cell.cdr;
+        }
+        return result + ")";
     }
 }
